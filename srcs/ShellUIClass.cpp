008dfba33bcbacd5a732d92b618268168fd0ab45
Alix Rive-Decaillot <rive.decaillot.a@gmail.com>
2019-01-20 20:24:04 +0100

    network push

--- srcs/ShellUIClass.cpp
+++ srcs/ShellUIClass.cpp
@@ -6,7 +6,7 @@
 /*   By: arive-de <arive-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/19 14:56:47 by jgourdin          #+#    #+#             */
-/*   Updated: 2019/01/20 20:06:52 by arive-de         ###   ########.fr       */
+/*   Updated: 2019/01/20 20:22:54 by arive-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ ShellUI::ShellUI(void)
 	this->_modules.push_back(new CpuModule());
     this->_modules.push_back(new OsInfosModule());
     this->_modules.push_back(new RamModule());
+    this->_modules.push_back(new NetworkModule());
     return;
 }
 
@@ -52,29 +53,6 @@ void    ShellUI::refresh(void) {
 
 }
 
-// void    ShellUI::refreshCpuUsage( void )
-// {
-//     if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO, reinterpret_cast<host_info_t>(&this->_cpuinfo), &this->_count) == KERN_SUCCESS)
-// 	{
-// 		this->_userTicks = this->_cpuinfo.cpu_ticks[0] - this->_prevUserTicks;
-// 		this->_cpuTicks = this->_cpuinfo.cpu_ticks[1] - this->_prevCpuTicks;
-// 		this->_idleTicks = this->_cpuinfo.cpu_ticks[2] - this->_prevIdleTicks;
-// 		this->_prevUserTicks = this->_cpuinfo.cpu_ticks[0];
-// 		this->_prevCpuTicks = this->_cpuinfo.cpu_ticks[1];
-// 		this->_prevIdleTicks = this->_cpuinfo.cpu_ticks[2];
-
-// 		mvwprintw(stdscr, 1, 2, "User : %.2f%%", static_cast<float>(this->_userTicks) / atoi(this->_coreCount.c_str()));
-// 		mvwprintw(stdscr, 2, 2, "Sys  : %.2f%%", static_cast<float>(this->_cpuTicks) / atoi(this->_coreCount.c_str()));
-// 		mvwprintw(stdscr, 3, 2, "Idle : %.2f%%", static_cast<float>(this->_idleTicks) / atoi(this->_coreCount.c_str()));
-// 	}
-
-// }
-
-// std::clock_t    IMonitorDisplay::getStart(void)
-// {
-//     return this->_start;
-// }
-
 void    ShellUI::init_display(void) {
 
     initscr();
@@ -93,7 +71,7 @@ void    ShellUI::init_display(void) {
     init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
 
     this->_winTitle = subwin(stdscr, 5, COLS - 2, 0, 0);
-    this->_winInfos = subwin(stdscr, 50, COLS - 2, 5, 0);
+    this->_winInfos = subwin(stdscr, 25, COLS - 2, 5, 0);
     box(this->_winTitle, ACS_VLINE, ACS_HLINE);
     box(this->_winInfos, ACS_VLINE, ACS_HLINE);
 
@@ -118,10 +96,14 @@ bool    ShellUI::duration()
 
 void    ShellUI::printModules(void) {
 
-    for (int i=0; i<6; i++)
-    {
-        mvwprintw(this->_winInfos, i + 3, 5, (this->_modules.at(i)->getInfos().c_str()));
-    }
+    mvwprintw(this->_winInfos, 3, 5, (this->_modules.at(0)->getInfos().c_str()));
+    mvwprintw(this->_winInfos, 6, 5, (this->_modules.at(1)->getInfos().c_str()));
+    mvwprintw(this->_winInfos, 9, 5, (this->_modules.at(2)->getInfos().c_str()));
+    mvwprintw(this->_winInfos, 12, 5, (this->_modules.at(3)->getInfos().c_str()));
+    mvwprintw(this->_winInfos, 15, 5, (this->_modules.at(4)->getInfos().c_str()));
+    mvwprintw(this->_winInfos, 18, 5, (this->_modules.at(5)->getInfos().c_str()));
+    mvwprintw(this->_winInfos, 21, 5, (this->_modules.at(6)->getInfos().c_str()));
+
 }
 
 
@@ -138,14 +120,6 @@ void    ShellUI::run_shellUI(void) {
         }
         if ((keycode = getch()) == ERR)
             continue;
-        else if (keycode == 'p')
-        {
-            std::cout << "previous" << std::endl;
-        }
-        else if (keycode == 'n')
-        {
-            std::cout << "next" << std::endl;           
-        }
     }
     endwin();
 }
