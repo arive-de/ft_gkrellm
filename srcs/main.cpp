2ca67d73682b4adbd2ffaf02e9f315e4544076c3
Johan GOURDIN <jgourdin@e2r4p10.42.fr>
2019-01-20 19:58:52 +0100

    wada

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jgourdin <jgourdin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/19 18:05:46 by arive-de          #+#    #+#             */
-/*   Updated: 2019/01/20 19:41:55 by jgourdin         ###   ########.fr       */
+/*   Updated: 2019/01/20 19:58:40 by jgourdin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,31 @@
 #include "GraphicalUIClass.hpp"
 #include "ShellUIClass.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
 	ShellUI shell;
-
-    //shell.init_display();
-
-	sf::RenderWindow window(sf::VideoMode(800, 700), "Text intro", sf::Style::Default);
-	window.setFramerateLimit(60);
-	GraphicalUI		ui(window);
-	while (window.isOpen())
+	if (argc != 2)
+		std::cout << "Please enter Shell or Graphics" << std::endl;
+	else if (argv[1] == "Shell")
+    	shell.init_display();
+	else if (argv[1] == "Graphics")
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		sf::RenderWindow window(sf::VideoMode(800, 700), "Text intro", sf::Style::Default);
+		window.setFramerateLimit(60);
+		GraphicalUI		ui(window);
+		while (window.isOpen())
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+			ui.render();
+			ui.refresh();
 		}
-		ui.render();
-		ui.refresh();
 	}
+	else
+		std::cout << "Please enter Shell or Graphics" << std::endl;
+	return ;
 }
