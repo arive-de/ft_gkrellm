d30633adec9b11991a10092a9051284951d16825
Alix Rive-Decaillot <rive.decaillot.a@gmail.com>
2019-01-20 17:46:46 +0100

    commit to pull

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgourdin <jgourdin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arive-de <arive-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/19 18:05:46 by jgourdin          #+#    #+#             */
-/*   Updated: 2019/01/20 17:23:25 by jgourdin         ###   ########.fr       */
+/*   Created: 2019/01/19 18:05:46 by arive-de          #+#    #+#             */
+/*   Updated: 2019/01/20 17:45:50 by arive-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,31 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
+#include "HostnameModule.hpp"
 #include "GraphicalUIClass.hpp"
+#include "ShellUIClass.hpp"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 700), "Text intro", sf::Style::Default);
-	window.setFramerateLimit(60);
-	GraphicalUI		ui(window);
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		ui.render();
-		ui.refresh();
-	}
+	ShellUI shell;
+
+    shell.init_display();
+
+	// sf::RenderWindow window(sf::VideoMode(800, 700), "Text intro", sf::Style::Default);
+	// window.setFramerateLimit(60);
+	// GraphicalUI		ui(window);
+	// while (window.isOpen())
+	// {
+	// 	sf::Event event;
+	// 	while (window.pollEvent(event))
+	// 	{
+	// 		if (event.type == sf::Event::Closed)
+	// 			window.close();
+	// 	}
+	// 	ui.render();
+	// 	ui.refresh();
+	// }
+
+
+
 }
