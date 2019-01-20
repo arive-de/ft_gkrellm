54b558dcb76a58dd6e3219d2db30eff0d26eb94c
Johan GOURDIN <jgourdin@e2r4p10.42.fr>
2019-01-20 18:33:57 +0100

    wad

--- srcs/CpuModule.cpp
+++ srcs/CpuModule.cpp
@@ -6,7 +6,7 @@
 /*   By: jgourdin <jgourdin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 14:25:03 by arive-de          #+#    #+#             */
-/*   Updated: 2019/01/20 17:30:21 by jgourdin         ###   ########.fr       */
+/*   Updated: 2019/01/20 18:33:42 by jgourdin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,15 +97,17 @@ unsigned long  CpuModule::getPrevIdleTicks(void)
 
 std::string          CpuModule::getInfos(void)
 {
-    this->_count = HOST_CPU_LOAD_INFO_COUNT;
-	if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO, reinterpret_cast<host_info_t>(&this->_cpuinfo), &this->_count) == KERN_SUCCESS)
-    {  
+
+    if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO, reinterpret_cast<host_info_t>(&this->_cpuinfo), &this->_count) == KERN_SUCCESS)
+    {
+        this->_userTicks = (this->_cpuinfo.cpu_ticks[0] - this->_prevUserTicks);
+        this->_cpuTicks = (this->_cpuinfo.cpu_ticks[1] - this->_prevCpuTicks);
+        this->_idleTicks = (this->_cpuinfo.cpu_ticks[2] - this->_prevIdleTicks);
         this->_prevUserTicks = this->_cpuinfo.cpu_ticks[0];
-		this->_prevCpuTicks = this->_cpuinfo.cpu_ticks[1];
-		this->_prevIdleTicks = this->_cpuinfo.cpu_ticks[2];
+        this->_prevCpuTicks = this->_cpuinfo.cpu_ticks[1];
+        this->_prevIdleTicks = this->_cpuinfo.cpu_ticks[2];
     }
-    
-    this->_cpuModule = this->_coreCount + "\n" + this->_clockSpeed + "\n" + this->_model;
+    this->_cpuModule = std::to_string(static_cast<float>(this->_userTicks) / 4) + "\n" + std::to_string(static_cast<float>(this->_cpuTicks) / 4) + "\n" + std::to_string(static_cast<float>(this->_idleTicks) / 4);\
     return this->_cpuModule;
 }
 
